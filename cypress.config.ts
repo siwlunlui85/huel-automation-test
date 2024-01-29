b0657ba49ddc8e1db1586a011cfa49967556129f
amy <amy@example.com>
2024-01-29 21:31:39 +0000

    Add Cypress Cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "8h4g1d",
     setupNodeEvents() {
       // implement node event listeners here
     },
